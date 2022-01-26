35af2f5acda3843440db7ad00d2e94c5c89f2c5c
masudrana <masudranaacb@gmail.com>
2022-01-26 23:41:54 +0600

    array details

---
+++ index.js
@@ -0,0 +1,8 @@
+//push & pop related array element add & remove in this arry
+var studants=[4,85,2,8,6,3,8,5];
+
+    studants.push('rifat');
+    console.log(studants);
+
+    studants.pop();
+    console.log(studants);
